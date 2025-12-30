304fa1c04cd04a5855b0b035e44a02956bf99c0f
Acsalas12 <varosiistvan10@gmail.com>
2025-12-30 13:17:13 +0100

    Initial commit

---
+++ data.py
@@ -0,0 +1,2 @@
+import os
+saves = r"C:\Users\local_user\AppData\LocalLow\Landfall Games\Content Warning\Saves"
---
+++ start_via_steam.bat
@@ -0,0 +1,8 @@
+@echo off
+:: Start the Manager
+start "" "CW_Manager.exe"
+
+:: Start Content Warning via Steam
+start steam://rungameid/2718170
+
+exit
